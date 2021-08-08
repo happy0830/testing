9a98f97c3c772c5aad8db9a98c5de4ff07a78777
happy0830 <happy600913@163.com>
2021-08-08 20:34:22 +0800

    test

--- testingdemo/test_cal.py
+++ testingdemo/test_cal.py
@@ -1,17 +1,39 @@
+
+
 import pytest
 import yaml
+import decimal
 
-@pytest.mark.parametrize(("a","b"),yaml.safe_load(open("./data.yaml")))
-class TestCal():
-    @pytest.mark.ADD
-    def test_add(self,a,b):
-        return a + b
-        print(a+b)
 
-    @pytest.mark.DIV
-    def test_div(self,a,b):
+class Cal():
+    def __int__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def add(self,a,b):
+        return a+b
+    def div(self,a,b):
         if b == 0:
             raise ZeroDivisionError
         else:
             return a/b
-            print(a/b)
+
+
+@pytest.mark.parametrize(("a","b"),yaml.safe_load(open("./data.yaml")))
+class TestCal():
+    # def setup(self):
+    #     self.cal=Cal()
+    # @pytest.mark.ADD
+    @pytest.mark.run(order=2)
+    def test_add(self,a,b):
+        result=Cal().add(a,b)
+        return result
+        print("result")
+
+    # @pytest.mark.DIV
+    @pytest.mark.run(order=1)
+    def test_div(self,a,b):
+        result= Cal().div(a,b)
+        return result
+        print(result)
+
